68e81103599c1a43e7af03c1a07f5840e15c713a
Ravi Kumar Tiwari <rkrtiwari@gmail.com>
2017-02-07 04:50:48 +0800

    added another example of regex parsing

--- informationExtraction.py
+++ informationExtraction.py
@@ -1,10 +1,25 @@
 import nltk
 import re
+
+### noun parsing
+grammer = """
+          NP: {<NN><NN>}"""
+grammer = "NP: {<NN>*}"
+
+cp = nltk.RegexpParser(grammer)    
+nouns = [('money', 'NN'), ('market', 'NN'), ('fund', 'NN')]
+
+
+
+## more complex noun parsing
 grammer = r"""
-       NP: {<DT|PP\$>?<JJ>*<NN>}{<NNP>+}
+       NP: {<DT|PP\$>?<JJ>*<NN>}
+       {<NNP>+}
        """
 cp = nltk.RegexpParser(grammer)
 sentence = [("Rapunzel", "NNP"), ("let", "VBD"), ("down", "RP"),
             ("her", "PP$"), ("long", "JJ"), ("golden", "JJ"),
             ("hair", "NN")]
 print cp.parse(sentence)
+
+
